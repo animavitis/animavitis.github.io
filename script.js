8cfad89b8c957f56dccbb4e63e408744f81c770f
Rafał Herok <rafal.herok@alan-systems.com>
2016-07-01 08:41:33 +0200

    next version

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 
+var watchface = {};
+var zoom = 4;
+
+
 function loadFile() {
     var input, file, fr;
-
     if (typeof window.FileReader !== 'function') {
         alert("The file API isn't supported on this browser yet.");
         return;
@@ -26,18 +29,30 @@ function loadFile() {
 
     function receivedText(e) {
         json = e.target.result;
-        var watchface = JSON.parse(json);
+        watchface = JSON.parse(json);
         console.log(watchface);
 
-        var zoom = $('#zoom').val();
+        zoom = $('#zoom').val();
         $("#watchface").css("width", 144 * zoom);
         $("#watchface").css("height", 168 * zoom);
         drawWatchface(watchface);
     }
 }
 
+function saveFile() {
+    var json_watchface = JSON.stringify(watchface);
+    var url = 'data:text/json;charset=utf8,' + encodeURIComponent(json_watchface);
+    window.open(url, '_blank');
+    window.focus();
+
+
+}
+
+
+
 function drawWatchface(data) {
     $("#watchface").html("");
+    $("#controlsList").html("");
 
     for (var i = 0; i < data.data.screens[0].controls.length; i++) {
         drawControl(data.data.screens[0].controls[i], i);
@@ -48,7 +63,7 @@ function drawWatchface(data) {
 
 function drawControl(controlData, index) {
 
-    var zoom = $('#zoom').val();
+
     var control = $("<div class=\"draggable\" id=\"control_" + index + "\"/>")
 
     if (controlData.type !== undefined) {
@@ -69,6 +84,7 @@ function drawControl(controlData, index) {
         control.append($("<span>x:" + control[0].offsetTop / zoom + "y:" + control[0].offsetLeft / zoom + "</span>"));
         control.append($("<br /><span>w:" + control[0].offsetWidth / zoom + "h:" + control[0].offsetHeight / zoom + "</span>"));
         control.prop("title", controlData.type + "_" + index);
+        control.prop("controlId", index);
         control.draggable({
             containment: "parent",
             grid: [4, 4],
@@ -76,6 +92,8 @@ function drawControl(controlData, index) {
                 var i = 5;
                 this.childNodes[0].innerText = "";
                 this.childNodes[0].innerText = "x:" + control[0].offsetTop / zoom + " y:" + control[0].offsetLeft / zoom;
+                watchface.data.screens[0].controls[control[0].controlId].position.x = control[0].offsetTop / zoom;
+                watchface.data.screens[0].controls[control[0].controlId].position.y = control[0].offsetLeft / zoom;
             }
         });
         control.resizable({
@@ -85,9 +103,48 @@ function drawControl(controlData, index) {
                 var i = 5;
                 this.childNodes[2].innerText = "";
                 this.childNodes[2].innerText = "w:" + control[0].offsetWidth / zoom + " h:" + control[0].offsetHeight / zoom;
+
+                if (watchface.data.screens[0].controls[control[0].controlId].type == "imageFromSet" || watchface.data.screens[0].controls[control[0].controlId].type == "number") {
+                    watchface.data.screens[0].controls[control[0].controlId].style.width = control[0].offsetWidth / zoom
+                    watchface.data.screens[0].controls[control[0].controlId].style.height = control[0].offsetHeight / zoom;
+
+                }
+                if (watchface.data.screens[0].controls[control[0].controlId].type == "text") {
+                    watchface.data.screens[0].controls[control[0].controlId].size.width = control[0].offsetWidth / zoom;
+                    watchface.data.screens[0].controls[control[0].controlId].size.height = control[0].offsetHeight / zoom
+                }
+
+
             }
         }).tooltip();
 
+        var controlsList = $("<div id=\"controlsList_" + index + "\"/>");
+        var coltrolsListCheckbox = $("<input type=\"checkbox\" name=\"my-checkbox\" id=\"checkbox_" + index + "\"/>");
+
+        $("#controlsList").append(controlsList);
+        controlsList.append(coltrolsListCheckbox);
+        controlsList.css("height", "50px");
+        coltrolsListCheckbox.bootstrapSwitch({
+            labelText: controlData.type + "_" + index,
+            labelWidth: 200,
+            state: "true",
+            handleWidth: 50,
+            onText: "displayed",
+            offText: "hidden",
+            size: "mini",
+            onSwitchChange: function (event, state) {
+                control.toggle();
+            }
+
+
+
+
+
+        });
+
+
     }
+
+
 }
 
